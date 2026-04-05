e7163ef5d0ad0b2449050465d74ab4d1ef696454
Emmanuel <capilitanemmanuel14@gmail.com>
2026-04-05 16:55:01 +0800

    fix build and path

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/dist' ,'index.html'));
+  res.sendFile(path.join(__dirname, 'client/dist/index.html'));
 });
 
 const router = require("./routes");
