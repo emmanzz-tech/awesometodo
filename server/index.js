3225d3c4a0a3fe8e3af2aed1ff750b8ddb31c29b
Emmanuel <capilitanemmanuel14@gmail.com>
2026-04-05 20:54:02 +0800

    Update my Render app

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const path = require('path');
 const app = express();
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'client/dist')));
-app.get("*", (req, res) => {
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
